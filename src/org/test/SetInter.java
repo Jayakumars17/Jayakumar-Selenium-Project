8bcab809c0b377f051b74af3fb9b36e8f939a93f
jayakumars17 <jayakumar270492@gmail.com>
2019-09-02 18:37:33 +0530

    Pushed the selenium code

---
+++ src/org/test/SetInter.java
@@ -0,0 +1,30 @@
+package org.test;
+
+import java.util.HashSet;
+import java.util.LinkedHashSet;
+import java.util.Set;
+import java.util.TreeSet;
+
+public class SetInter {
+
+	public static void main(String[] args) {
+		Set<Character> c=new HashSet<>();
+		c.add('a');
+		c.add('A');
+		c.add('1');
+		c.add('&');
+		c.add(' ');
+		System.out.println(c);
+		Set<Character> d=new LinkedHashSet<>();
+		d.add('A');
+		d.add('a');
+		d.add('E');
+		System.out.println(d);
+		Set<Character> e=new TreeSet();
+		e.addAll(c);
+		System.out.println(e);
+		
+		
+	}
+
+}
