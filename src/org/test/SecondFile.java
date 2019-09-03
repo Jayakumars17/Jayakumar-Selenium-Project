42b4cd547cbfd849786614a7c6b01b62a9ddb7c0
jayakumars17 <jayakumar270492@gmail.com>
2019-09-03 18:21:42 +0530

    New java file added

---
+++ src/org/test/SecondFile.java
@@ -0,0 +1,19 @@
+package org.test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SecondFile {
+
+	public static void main(String[] args) {
+		List<Integer> c=new ArrayList<Integer>();
+		c.add(10);
+		c.add(20);
+		c.add(40);
+		c.add(60);
+		c.add(80);
+		System.out.println(c);
+
+	}
+
+}
